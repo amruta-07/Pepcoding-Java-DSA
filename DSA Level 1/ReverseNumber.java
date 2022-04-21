9b0ebec412cf0f045b95643459860fe6e0d5a183
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-04-21 20:22:41 +0530

    Add files via upload

--- DSA Level 1/ReverseNumber.java
+++ DSA Level 1/ReverseNumber.java
@@ -1,1 +1,18 @@
+package Pepcode;
 
+import java.util.Scanner;
+
+public class ReverseNumber {
+	public static void main(String [] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int n = sc.nextInt();
+		 //reverse a  given number
+		while(n>0) {
+			int dig=n%10;
+			n=n/10;
+
+			System.out.print(dig);
+		}
+	}
+}
