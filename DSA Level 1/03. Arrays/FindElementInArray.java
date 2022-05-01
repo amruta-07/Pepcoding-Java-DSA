7490c48067a341d00c49eeee7fed343792e1306c
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-05-02 00:42:50 +0530

    Add files via upload

--- DSA Level 1/03. Arrays/FindElementInArray.java
+++ DSA Level 1/03. Arrays/FindElementInArray.java
@@ -1,1 +1,34 @@
+package Pepcode;
 
+import java.util.Scanner;
+
+public class FindElementInArray {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int []arr = new int[n];
+		for(int i = 0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+			
+		}
+		int data = sc.nextInt();
+		int idx = -1;
+		
+		for(int i= 0; i<arr.length; i++) {
+			if(arr[i] == data) {
+				idx = i;
+				break;
+			}
+		}
+		System.out.println(idx);
+		
+		
+		
+	}
+
+}
+//input: 3
+//       1
+//       2
+//       3
+//       2   if 2 is in given array element then print index else -1 so the output is 1.
