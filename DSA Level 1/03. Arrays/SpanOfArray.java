3cbb8d5f9ee1ac5dd93be3728927ee5ae316cb3f
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-05-02 00:40:25 +0530

    Add files via upload

--- DSA Level 1/03. Arrays/SpanOfArray.java
+++ DSA Level 1/03. Arrays/SpanOfArray.java
@@ -1,1 +1,30 @@
+package Pepcode;
 
+import java.util.Scanner;
+
+public class SpanOfArray {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int []arr = new int[n];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int max = arr[0];
+		int min = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+				
+			}
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		int span = max - min;
+		System.out.println(span);
+	}
+
+}
