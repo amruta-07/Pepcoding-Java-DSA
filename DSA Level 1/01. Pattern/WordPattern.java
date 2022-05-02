2df5172020a19fb0e31780cd81f79ff4b42c8f6f
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-05-03 00:29:02 +0530

    Add files via upload

--- DSA Level 1/01. Pattern/WordPattern.java
+++ DSA Level 1/01. Pattern/WordPattern.java
@@ -1,1 +1,31 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
 
+        String[] S = s.split("\\s");
+        int n = pattern.length();
+        int m = S.length;
+        if (n != m)
+            return false;
+
+        Map<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+
+            if (!map.containsKey(pattern.charAt(i))) {
+
+                if (map.containsValue(S[i]))
+                    return false;
+                map.put(pattern.charAt(i), S[i]);
+
+            } else {
+
+                if (!map.get(pattern.charAt(i)).equals(S[i]))
+                    return false;
+
+            }
+        }
+
+        return true;
+
+    }
+}
